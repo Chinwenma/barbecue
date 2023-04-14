d2d30e2ce68a419c06fc6e23a33a6b0b9ae593f4
Chinwenma <okoriechinwe1327@gmail.com>
2023-04-13 19:41:00 -0700

    building

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,17 @@
 export default function About(){
     return(
-        <div className="bg-gradient-to-br from-orange-500 to-red-500 absolute h-72 w-full  rounded-none">
+        <div className="bg-gradient-to-br from-orange-500 to-red-500 absolute h-96 w-full  rounded-none">
+<h1 className="font-butler font-bold text-5xl leading-tight tracking-tighter text-white capitalize">
+About Bigk Grills
+</h1>
+<div>
+    <div>
 
+    </div>
+    <div>
+        
+    </div>
+</div>
         </div>
     )
 }
