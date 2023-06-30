2fffdd6cf96e04ef2cdafaa3fdeb949acae8d77f
Chinwenma <okoriechinwe1327@gmail.com>
2023-06-30 12:05:10 -0800

    g

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
             <div className="container flex flex-col justify-center items-center px-6 mx-auto mt-6 space-y-0 md:space-y-10 md:flex-row sm:px-6 lg:px-8 ">
                
 
-                <div className="flex  flex-row justify-between items-center  gap-52 mt-12 ">
+                <div className="flex  flex-col justify-between items-center  mt-12  gap-8 md:gap-52 md:flex-row">
                     <div className="flex flex-col items-center  bg-white w-64  space-x-4 text-center justify-center">
                         <img
                             className=" w-34  object-cover px-4 py-4"
@@ -18,16 +18,22 @@ const About = () => {
                             alt="Profile"
                         />
                         <p className="  mt-4 text-lg leading-tight font-semibold tracking-tight text-start ">Olusegun Obasanjo</p>
-                        <p className="mt-2 text-gray-600 text-2xl leading-tight tracking-tighter" >C. E. O</p>
+                        <p className="mt-2 text-gray-600 text-2xl leading-tight tracking-tighter text-start" >C. E. O</p>
 
                     </div>
-                    <div className='bg-gray-600' style={{ width: "1px", height: "300px" }}></div>
+                    <div className='hidden md:flex bg-white' style={{ width: "1px", height: "300px" }}></div>
                     <div>
-                        <p className="text-gray-600 max-w-sm">
+                        <p className="text-white max-w-sm md:max-w-md lg:max-w-lg">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
                             eget mauris velit. Morbi tempor lectus felis. Curabitur ac enim
                             fermentum, interdum turpis at, accumsan mi. Donec egestas ligula
                             non nunc interdum, nec feugiat lectus consectetur.
+                            orem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                            eget mauris velit. Morbi tempor lectus felis. Curabitur ac enim
+                            fermentum, interdum turpis at, accumsan
+                            orem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                            eget mauris velit. Morbi tempor lectus felis. Curabitur ac enim
+                            fermentum, interdum turpis at, accumsan
                         </p>
                     </div>
                 </div>
