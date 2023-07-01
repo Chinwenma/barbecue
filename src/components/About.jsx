c2d4a3c6726fb47169a4a94e8f4353593ac048f1
Chinwenma <okoriechinwe1327@gmail.com>
2023-06-30 16:44:17 -0800

    AboutDone

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,13 +4,13 @@ import CEO from "../assets/ceo.png"
 const About = () => {
     return (
         <div className="bg-gradient-to-br from-orange-500 to-red-500 w-full py-8">
-             <h1 className="text-3xl leading-9 font-extrabold text-white sm:text-4xl sm:leading-10">
-                    About Bigk Grills
-                </h1>
-            <div className="container flex flex-col justify-center items-center px-6 mx-auto mt-6 space-y-0 md:space-y-10 md:flex-row sm:px-6 lg:px-8 ">
-               
+            <h1 className="text-3xl text-center leading-9 font-extrabold pl-6 text-white sm:text-4xl  md:text-center lg:text-start sm:leading-10">
+                About Bigk Grills
+            </h1>
+            <div className="container flex flex-col justify-center items-center px-6 mx-auto mt-6 space-y-0 md:space-y-6 md:flex-row sm:px-6 lg:px-8 ">
 
-                <div className="flex  flex-col justify-between items-center  mt-12  gap-8 md:gap-52 md:flex-row">
+
+                <div className="flex  flex-col justify-between items-center text-right mt-12 gap-8   lg:gap-36 md:flex-row">
                     <div className="flex flex-col items-center  bg-white w-64  space-x-4 text-center justify-center">
                         <img
                             className=" w-34  object-cover px-4 py-4"
@@ -23,17 +23,10 @@ const About = () => {
                     </div>
                     <div className='hidden md:flex bg-white' style={{ width: "1px", height: "300px" }}></div>
                     <div>
-                        <p className="text-white max-w-sm md:max-w-md lg:max-w-lg">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                            eget mauris velit. Morbi tempor lectus felis. Curabitur ac enim
-                            fermentum, interdum turpis at, accumsan mi. Donec egestas ligula
-                            non nunc interdum, nec feugiat lectus consectetur.
-                            orem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                            eget mauris velit. Morbi tempor lectus felis. Curabitur ac enim
-                            fermentum, interdum turpis at, accumsan
-                            orem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                            eget mauris velit. Morbi tempor lectus felis. Curabitur ac enim
-                            fermentum, interdum turpis at, accumsan
+                        <p className="text-white text-justify max-w-sm md:max-w-md lg:max-w-lg md:text-start md:text-xl ">
+                            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+
+                            elit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.elit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.elit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.elit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
                         </p>
                     </div>
                 </div>
