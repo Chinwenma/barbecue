b51a16912c7d424043e777976c6ac6fd7187f1da
Chinwenma <okoriechinwe1327@gmail.com>
2023-07-01 10:44:10 -0800

    footer

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-
+import MAP from '../assets/Rectangle 16.png';
 
 const Contact = () => {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Contact Us</h1>
-      <div className="flex flex-wrap">
-        <div className="w-full md:w-1/2 lg:w-1/3 px-2 mb-4">
-          <img src="map-of-san-francisco.png" alt="Map of San Francisco" className="w-full h-auto" />
+      <div className="flex   justify-center items-center flex-wrap">
+        <div className="w-full md:w-1/2 lg:w-1/2 px-2 mb-4">
+          <img src={MAP} alt="Map of San Francisco" className="w-full h-auto" />
         </div>
-        <div className="w-full md:w-1/2 lg:w-2/3 px-2 mb-4">
+        <div className="w-full md:w-1/2 lg:w-1/2 px-2 mb-4">
           <div className="mb-4">
             <h2 className="text-lg font-semibold">Phone:</h2>
             <p className="text-gray-600">123-456-7890</p>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
-
+import React from 'react';
+import Logo  from"../assets/logo.png"
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="bg-gradient-to-br from-orange-500 to-red-500 text-white py-8">
+      <div className="container mx-auto flex flex-wrap justify-center">
+      
+        <div className='w-full md:w-1/4 px-4 mb-4 md:mb-0'>
+        <img src={Logo} alt=" app logo"  className=" w-12 h-12 md:w-20 md:h-20"/>
+      </div>
+        <div className="w-full md:w-1/4 px-4 mb-4 md:mb-0">
+          <h4 className="font-bold mb-2">Location</h4>
+          <p>123 Main St, San Francisco, CA</p>
+        </div>
+        <div className="w-full md:w-1/4 px-4 mb-4 md:mb-0">
+          <h4 className="font-bold mb-2">Social</h4>
+          <ul className="list-none">
+            <li className="mb-2">
+              <a href="/" className=" hover:text-slate-900">
+                Facebook
+              </a>
+            </li>
+            <li className="mb-2">
+              <a href="/" className=" hover:text-slate-900">
+                Twitter
+              </a>
+            </li>
+            <li className="mb-2">
+              <a href="/" className=" hover:text-slate-900">
+                Instagram
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="w-full md:w-1/4 px-4 mb-4 md:mb-0">
+          <h4 className="font-bold mb-2">Links</h4>
+          <ul className="list-none">
+            <li className="mb-2">
+              <a href="/" className=" hover:text-slate-900">
+                About Us
+              </a>
+            </li>
+            <li className="mb-2">
+              <a href="/" className="t hover:text-slate-900">
+                Contact Us
+              </a>
+            </li>
+            <li className="mb-2">
+              <a href="/" className=" hover:text-slate-900">
+                Privacy Policy
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="container mx-auto mt-8 text-center">
+        <p className="text-sm text-slate-600">&copy; 2023 Your Company. All rights reserved.</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
