fe70888301b5da3dd8be1975b768494fb981ab9e
Chinwenma <okoriechinwe1327@gmail.com>
2023-07-03 23:33:09 -0800

    landing

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,149 +1,13 @@
-import React, { useState } from 'react';
-import { data } from '../constants';
-
-const MenuCard = ({ item: { pic, title, price } }) => (
-  <div className="flex flex-col items-center mb-8">
-    <img src={pic} alt="item" className="w-20 h-20 mb-2" />
-    <div>
-      <p className="text-lg font-bold">{title}</p>
-      <p className="text-gray-500">{price}</p>
-    </div>
-  </div>
-);
-
-const Menu = () => {
-  const [activeMenu, setActiveMenu] = useState('Turkeys');
-
-  const handleMenuClick = (menu) => {
-    setActiveMenu(menu);
-  };
-
-  const getMenuData = () => {
-    switch (activeMenu) {
-      case 'Turkeys':
-        return data.Turkeys;
-      case 'Fishes':
-        return data.Fishes;
-      case 'Drinks':
-        return data.Drinks;
-      case 'Barbeques':
-        return data.Barbeques;
-      default:
-        return [];
-    }
-  };
-
-  return (
-    <div className="bg-gray-100 p-4">
-      <h1 className="text-3xl font-bold mb-4">Our Menu</h1>
-      <div className="flex flex-row items-center justify-center mb-4">
-        <ul className="flex flex-row text-base items-baseline w-full md:w-auto mb-4 md:mb-0 md:flex-col">
-          <li
-            className={`mr-4 md:mr-0 md:mb-4 font-dm text-lg hover:text-orange-600 hover:border-b-2 hover:border-orange-500 transition-all duration-300 cursor-pointer ${
-              activeMenu === 'Turkeys' ? 'font-bold' : ''
-            }`}
-            onClick={() => handleMenuClick('Turkeys')}
-          >
-            Turkeys
-          </li>
-          <li
-            className={`mr-4 md:mr-0 md:mb-4 font-dm text-lg hover:text-orange-600 hover:border-b-2 hover:border-orange-500 transition-all duration-300 cursor-pointer ${
-              activeMenu === 'Fishes' ? 'font-bold' : ''
-            }`}
-            onClick={() => handleMenuClick('Fishes')}
-          >
-            Fishes
-          </li>
-          <li
-            className={`mr-4 md:mr-0 md:mb-4 font-dm text-lg hover:text-orange-600 hover:border-b-2 hover:border-orange-500 transition-all duration-300 cursor-pointer ${
-              activeMenu === 'Drinks' ? 'font-bold' : ''
-            }`}
-            onClick={() => handleMenuClick('Drinks')}
-          >
-            Drinks
-          </li>
-          <li
-            className={`mr-4 md:mr-0 md:mb-4 font-dm text-lg hover:text-orange-600 hover:border-b-2 hover:border-orange-500 transition-all duration-300 cursor-pointer ${
-              activeMenu === 'Barbeques' ? 'font-bold' : ''
-            }`}
-            onClick={() => handleMenuClick('Barbeques')}
-          >
-            Barbeques
-          </li>
-        </ul>
-        <div className="h-8 bg-gray-300 w-5 md:hidden" />
-      </div>
-      <div className="flex flex-wrap justify-center gap-8">
-        {getMenuData().map((item, index) => (
-          <MenuCard item={item} key={index} />
-        ))}
-      </div>
-      <div className="flex justify-center mt-8">
-        <ul className="flex items-center space-x-2">
-          {getMenuData().map((_, index) => (
-            <li
-              key={index}
-              className={`w-4 h-4 rounded-full bg-gray-400 ${
-                index === 0 ? 'bg-gray-800' : ''
-              }`}
-            >
-              {index + 1}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
-};
-
-export default Menu;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // import React, { useState } from 'react';
 // import { data } from '../constants';
 
-// const MenuCard = ({ item: { pic, title, price } }) => (
-//   <div className="flex items-center mb-4">
-//     <img src={pic} alt="item" className="w-20 h-20 mr-4" />
+// const MenuCard = ({ item: { pic, title, subTiltle, price } }) => (
+//   <div className="flex flex-col items-center mb-8 p-4 bg-white shadow">
+//     <img src={pic} alt="item" className="w-20 h-20 mb-2" />
 //     <div>
 //       <p className="text-lg font-bold">{title}</p>
 //       <p className="text-gray-500">{price}</p>
+//       <p className="text-gray-700">{subTiltle}</p>
 //     </div>
 //   </div>
 // );
@@ -172,11 +36,12 @@ export default Menu;
 
 //   return (
 //     <div className="bg-gray-100 p-4">
-//       <h1 className="text-3xl font-bold mb-4 text-left">Our Menu</h1>
-//       <div className="flex flex-row items-center justify-center mb-4">
-//         <ul className="flex flex-row  text-base items-baseline w-full md:w-auto mb-4 md:mb-0 md:flex-col">
+//       <h1 className="text-3xl font-bold mb-4">Our Menu</h1>
+//       <div className="flex flex-row items-center justify-between mb-4">
+//         <div className='flex mt-0'>
+//         <ul className="flex flex-row text-base w-full md:w-auto mb-4 md:mb-0 md:flex-col">
 //           <li
-//             className={`mr-4 md:mr-0 md:mb-4  font-dm text-lg hover:text-orange-600 hover:border-b-2 hover:border-orange-500 transition-all duration-300 cursor-pointer ${
+//             className={`mr-4 md:mr-0 md:mb-4 font-dm text-lg hover:text-orange-600 hover:border-b-2 hover:border-orange-500 transition-all duration-300 cursor-pointer ${
 //               activeMenu === 'Turkeys' ? 'font-bold' : ''
 //             }`}
 //             onClick={() => handleMenuClick('Turkeys')}
@@ -208,22 +73,32 @@ export default Menu;
 //             Barbeques
 //           </li>
 //         </ul>
-//         <div className="h-8 bg-gray-300 w-5  md:hidden" />
-//         <div className="flex flex-wrap w-full">
-//           {getMenuData().map((item, index) => (
-//             <MenuCard item={item} key={index} />
-//           ))}
 //         </div>
+//         <div
+//           className="hidden md:flex bg-gray-700"
+//           style={{ width: '1px', height: '300px' }}
+//         ></div>
+
+//       <div className="flex flex-wrap justify-center gap-4 md:w-1/2 lg:w-3/4">
+//         {getMenuData().map((item, index) => (
+//           <MenuCard item={item} key={index} />
+//         ))}
 //       </div>
-//       <div className="flex justify-center">
+
+//       </div>
+
+//       <div className="flex justify-center mt-8">
 //         <ul className="flex items-center space-x-2">
-//           {Array.from({ length: Math.ceil(getMenuData().length / 4) }).map((_, index) => (
+//           {Array.from({ length: 4 }).map((_, index) => (
 //             <li
 //               key={index}
-//               className={`w-4 h-4 rounded-full bg-gray-400 ${
+//               className={`w-4 h-4 p-3 rounded-full text-center bg-gray-400 ${
 //                 index === 0 ? 'bg-gray-800' : ''
-//               }`}
-//             />
+//               } cursor-pointer`}
+//               onClick={() => handleMenuClick(Object.keys(data)[index])}
+//             >
+//               {index + 1}
+//             </li>
 //           ))}
 //         </ul>
 //       </div>
@@ -232,3 +107,164 @@ export default Menu;
 // };
 
 // export default Menu;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React, { useState } from 'react';
+import { data } from '../constants';
+
+const MenuCard = ({ item: { pic, title, subTitle, price } }) => (
+  <div className="flex flex-col items-start mb-6 p-2 bg-gray-100 shadow  text-left md:w-1/4">
+    <img src={pic} alt="item" className="w-18 h-18 mb-2" />
+    <div>
+      <p className="text-lg font-bold">{title}</p>
+      <p className='text-gray-700'>{subTitle}</p>
+      <p className="text-gray-500">{price}</p>
+      
+    </div>
+  </div>
+);
+
+const Menu = () => {
+  const [activeMenu, setActiveMenu] = useState('Turkeys');
+
+  const handleMenuClick = (menu) => {
+    setActiveMenu(menu);
+  };
+
+  const getMenuData = () => {
+    switch (activeMenu) {
+      case 'Turkeys':
+        return data.Turkeys;
+      case 'Fishes':
+        return data.Fishes;
+      case 'Drinks':
+        return data.Drinks;
+      case 'Barbeques':
+        return data.Barbeques;
+      default:
+        return [];
+    }
+  };
+
+  return (
+    <div className="bg-gray-200 p-4">
+      <h1 className="text-3xl font-bold mb-4">Our Menu</h1>
+      <div className="flex flex-col items-center justify-center mb-4 md:flex-row md:gap-4 md:relative">
+        <div className='md:absolute top-0 left-4 '>
+        <ul className="flex flex-row text-base w-full md:w-auto mb-4 md:mb-0 md:flex-col sm:flex-row">
+          <li
+            className={`mr-4 md:mr-0 md:mb-4 font-dm text-lg hover:text-orange-600 hover:border-b-2 hover:border-orange-500 transition-all duration-300 cursor-pointer ${activeMenu === 'Turkeys' ? 'font-bold' : ''
+              }`}
+            onClick={() => handleMenuClick('Turkeys')}
+          >
+            Turkeys
+          </li>
+          <li
+            className={`mr-4 md:mr-0 md:mb-4 font-dm text-lg hover:text-orange-600 hover:border-b-2 hover:border-orange-500 transition-all duration-300 cursor-pointer  ${activeMenu === 'Fishes' ? ' active:text-orange-600' : ''
+              }`}
+            onClick={() => handleMenuClick('Fishes')}
+          >
+            Fishes
+          </li>
+          <li
+            className={`mr-4 md:mr-0 md:mb-4 font-dm text-lg hover:text-orange-600 hover:border-b-2 hover:border-orange-500 transition-all duration-300 cursor-pointer ${activeMenu === 'Drinks' ? 'font-bold' : ''
+              }`}
+            onClick={() => handleMenuClick('Drinks')}
+          >
+            Drinks
+          </li>
+          <li
+            className={`mr-4 md:mr-0 md:mb-4 font-dm text-lg hover:text-orange-600 hover:border-b-2 hover:border-orange-500 transition-all duration-300 cursor-pointer ${activeMenu === 'Barbeques' ? 'font-bold' : ''
+              }`}
+            onClick={() => handleMenuClick('Barbeques')}
+          >
+            Barbeques
+          </li>
+        </ul>
+        <div className='hidden md:flex bg-gray-700 md:absolute top-0 left-24 ' style={{ width: "1px", height: "300px" }}></div>
+        </div>
+        <div className="flex flex-wrap justify-center gap-8">
+          {getMenuData().map((item, index) => (
+            <MenuCard item={item} key={index} />
+          ))}
+        </div>
+
+      </div>
+      <div className="flex justify-center mt-8">
+  <ul className="flex items-center space-x-2">
+    {Array.from({ length: 4 }).map((_, index) => (
+      <li
+        key={index}
+        className={`w-6 h-6 pb-3 rounded-full text-center bg-gray-400 ${
+          index === 0 ? 'bg-orange-500' : ''
+        } cursor-pointer`}
+        onClick={() => handleMenuClick(Object.keys(data)[index])}
+      >
+      {index + 1}
+      </li>
+    ))}
+  </ul>
+</div>
+
+      {/* <div className="flex justify-center mt-8">
+        <ul className="flex items-center space-x-2">
+          {Array.from({ length: 4 }).map((_, index) => (
+            <li
+              key={index}
+              className={`w-4 h-4 p-3 rounded-full text-center bg-gray-400 ${index === 0 ? 'bg-gray-800' : ''
+                } cursor-pointer`}
+              onClick={() => handleMenuClick(Object.keys(data)[index])}
+            >
+              {index + 1}
+            </li>
+          ))}         </ul>
+      </div> */}
+     
+    </div>
+  );
+};
+
+export default Menu;
--- src/constants/data.js
+++ src/constants/data.js
@@ -4,90 +4,127 @@ import images from "./images";
 const Turkeys = [
   {
     pic: images.menuA,
-    title: 'Buttermilk Crispy Chicken Sandwich Meal',
+    title: 'Buttermilk Crispy ',
+    subTitle: 'Chicken Sandwich Meal',
     price: 'N2,450',
   },
   {
     pic: images.menuB,
-    title: 'Buttermilk Crispy Chicken Sandwich',
+
+    title: 'Buttermilk Crispy ',
+    subTitle: 'Chicken Sandwich Meal',
+
     price: 'N2,450',
   },
   {
     pic: images.menuA,
-    title: 'Buttermilk Crispy Chicken Sandwich Meal',
+
+    title: 'Buttermilk Crispy',
+    subTitle: 'Chicken Sandwich Meal',
+
     price: 'N2,450',
   },
   {
     pic: images.menuC,
-    title: 'Buttermilk Crispy Chicken Sandwich Meal',
+
+    title: 'Buttermilk Crispy',
+    subTitle: 'Chicken Sandwich Meal',
+
     price: 'N2,450',
   },
   {
     pic: images.menuD,
-    title: 'Buttermilk Crispy Chicken Sandwich Meal',
+
+    title: 'Buttermilk Crispy',
+    subTitle: 'Chicken Sandwich Meal',
+
     price: 'N2,450',
   },
   {
     pic: images.menuA,
-    title: 'Buttermilk Crispy Chicken Sandwich Meal',
+    title: 'Buttermilk Crispy',
+    subTitle: 'Chicken Sandwich Meal',
+
     price: 'N2,450',
   },
   {
     pic: images.menuB,
-    title: 'Buttermilk Crispy Chicken Sandwich Meal',
+    title: 'Buttermilk Crispy',
+    subTitle: 'Chicken Sandwich Meal',
+
     price: 'N2,450',
   },
   {
     pic: images.menuA,
-    title: 'Buttermilk Crispy Chicken Sandwich Meal',
+    title: 'Buttermilk Crispyl',
+    subTitle: 'Chicken Sandwich Meal',
+
     price: 'N2,450',
   },
   {
     pic: images.menuC,
-    title: 'Buttermilk Crispy Chicken Sandwich Meal',
+    title: 'Buttermilk Crispyl',
+    subTitle: 'Chicken Sandwich Meal',
+
     price: 'N2,450',
   },
   {
     pic: images.menuD,
-    title: 'Buttermilk Crispy Chicken Sandwich Meal',
+    title: 'Buttermilk Crispyl',
+    subTitle: 'Chicken Sandwich Meal',
+
     price: 'N2,450',
   },
   {
     pic: images.menuC,
-    title: 'Buttermilk Crispy Chicken Sandwich Meal',
+    title: 'Buttermilk Crispyl',
+    subTitle: 'Chicken Sandwich Meal',
+
     price: 'N2,450',
   },
   {
     pic: images.menuB,
-    title: 'Buttermilk Crispy Chicken Sandwich Meal',
+    title: 'Buttermilk Crispyl',
+    subTitle: 'Chicken Sandwich Meal',
+
     price: 'N2,450',
   },
   {
     pic: images.menuA,
-    title: 'Buttermilk Crispy Chicken Sandwich Meal',
+    title: 'Buttermilk Crispy ',
+    subTitle: 'Chicken Sandwich Meal',
+
     price: 'N2,450',
   },
 ];
 
 const Fishes = [
   {
-    pic : images.fishA,
-    title: 'Buttermilk Crispy Chicken Sandwich Meal',
+    pic: images.fishA,
+    title: 'Buttermilk Crispy',
+    subTitle: 'Chicken Sandwich Meal',
+
     price: 'N3,450',
   },
   {
     pic: images.fishB,
-    title: 'Buttermilk Crispy Chicken Sandwich',
+    title: 'Buttermilk Crispy ',
+    subTitle: 'Chicken Sandwich Meal',
+
     price: 'N4,450',
   },
   {
     pic: images.fishC,
     title: 'CatFish and Tilapis',
+    subTitle: 'Chicken Sandwich Meal',
+
     price: 'N5,450',
   },
   {
     pic: images.fishD,
     title: 'Fish PepperSoup',
+    subTitle: 'Chicken Sandwich Meal',
+
     price: 'N6,450',
   },
 ];
@@ -96,21 +133,29 @@ const Drinks = [
   {
     pic: images.drinkA,
     title: 'Andre',
+    subTitle: 'Chicken Sandwich Meal',
+
     price: 'N12,450',
   },
   {
     pic: images.drinkB,
     title: 'veletas',
+    subTitle: 'Chicken Sandwich Meal',
+
     price: 'N211,450',
   },
   {
     pic: images.drinkC,
     title: 'Pure Heaven',
+    subTitle: 'Chicken Sandwich Meal',
+
     price: 'N8,450',
   },
   {
     pic: images.drinkD,
     title: 'Eva Wine',
+    subTitle: 'Chicken Sandwich Meal',
+
     price: 'N450',
   },
 ];
@@ -118,52 +163,72 @@ const Drinks = [
 const Barbeques = [
   {
     pic: images.barA,
-    title: 'Buttermilk Crispy Chicken Sandwich Meal',
+    title: 'Buttermilk Crispy',
+    subTitle: 'Chicken Sandwich Meal',
+
     price: 'N122,450',
   },
   {
     pic: images.barB,
-    title: 'Buttermilk Crispy Chicken Sandwich',
+    title: 'Buttermilk Crispy',
+    subTitle: 'Chicken Sandwich Meal',
+
     price: 'N27,450',
   },
   {
     pic: images.barC,
-    title: 'Buttermilk Crispy Chicken Sandwich Meal',
+    title: 'Buttermilk Crispy',
+    subTitle: 'Chicken Sandwich Meal',
+
     price: 'N28,450',
   },
   {
     pic: images.barD,
-    title: 'Buttermilk Crispy Chicken Sandwich Meal',
+    title: 'Buttermilk Crispy',
+    subTitle: 'Chicken Sandwich Meal',
+
     price: 'N34,450',
   },
   {
     pic: images.barE,
-    title: 'Buttermilk Crispy Chicken Sandwich Meal',
+    title: 'Buttermilk Crispy',
+    subTitle: 'Chicken Sandwich Meal',
+
     price: 'N34,450',
   },
   {
     pic: images.barF,
-    title: 'Buttermilk Crispy Chicken Sandwich Meal',
+    title: 'Buttermilk Crispy',
+    subTitle: 'Chicken Sandwich Meal',
+
     price: 'N34,450',
   },
   {
     pic: images.barG,
-    title: 'Buttermilk Crispy Chicken Sandwich Meal',
+    title: 'Buttermilk Crispy',
+    subTitle: 'Chicken Sandwich Meal',
+
     price: 'N34,450',
   },
   {
     pic: images.barH,
-    title: 'Buttermilk Crispy Chicken Sandwich Meal',
+    title: 'Buttermilk Crispy',
+    subTitle: 'Chicken Sandwich Meal',
+
     price: 'N34,450',
   },
   {
     pic: images.barL,
-    title: 'Buttermilk Crispy Chicken Sandwich Meal',
+    title: 'Buttermilk Crispy',
+    subTitle: 'Chicken Sandwich Meal',
+
     price: 'N34,450',
   },
   {
     pic: images.barM,
-    title: 'Buttermilk Crispy Chicken Sandwich Meal',
+    title: 'Buttermilk Crispy',
+    subTitle: 'Chicken Sandwich Meal',
+
     price: 'N34,450',
   },
 ];
